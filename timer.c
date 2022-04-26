79bd0ce0e46770198eb23633c53821ceeecbae82
Jorim <s8jmbech@stud.uni-saarland.de>
2022-04-26 10:47:52 +0200

    writing to file

--- timer.c
+++ timer.c
@@ -5,9 +5,12 @@
 #include <unistd.h>
 
 void main() {
+    FILE *fp;
+    fp = fopen("log.txt", "w");
     for (int i = 0; i < 100; i++) {
         uint64_t val;
         asm volatile ("rdcycle %0\n":"=r" (val) ::);
-        printf("%lu\n", val);
+        fprintf(fp, "%lu\n", val);
     }
+    fclose(fp);
 }
