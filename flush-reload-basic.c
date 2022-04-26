419467011b4e09793278fb0d2cacb558881dcd93
Jorim <s8jmbech@stud.uni-saarland.de>
2022-04-26 15:57:54 +0200

    revert

--- flush-reload-basic.c
+++ flush-reload-basic.c
@@ -28,7 +28,7 @@ static inline uint64_t rdtsc()
 // On our processor this is not sufficient -> eviction sets are necessary
 static inline void flush()
 {
-    asm volatile("sfence.vma" ::
+    asm volatile("fence.i" ::
                      : "memory");
     asm volatile("fence" ::
                      : "memory");
