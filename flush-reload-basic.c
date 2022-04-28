8bf9cd3494c1c49f948d748fa88cca013f0f0294
Jorim <s8jmbech@stud.uni-saarland.de>
2022-04-28 11:06:40 +0200

    fixed pointers

--- flush-reload-basic.c
+++ flush-reload-basic.c
@@ -93,10 +93,10 @@ void main()
          Time both array indices and pick the one with the smaller time i.e
          the one that is in cache
         */
-        printf("%p\n", victim_arr[0]);
-        printf("%p\n", victim_arr[1023]);
-        timings[0] = timed_load(victim_arr[0]);
-        timings[1] = timed_load(victim_arr[1023]);
+        printf("%p\n", &victim_arr[0]);
+        printf("%p\n", &victim_arr[1023]);
+        timings[0] = timed_load(&victim_arr[0]);
+        timings[1] = timed_load(&victim_arr[1023]);
         printf("%ld %ld: ", timings[0], timings[1]);
         if (timings[0] < timings[1]) {
             printf("0\n");
