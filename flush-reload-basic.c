4e0c2d787256ea4486be38e2bec76889e5bea25e
Jorim <s8jmbech@stud.uni-saarland.de>
2022-04-29 14:23:23 +0200

    cleanup

--- flush-reload-basic.c
+++ flush-reload-basic.c
@@ -62,13 +62,9 @@ static inline uint64_t timed_call(uint64_t (*p)(uint64_t, uint64_t))
 static inline uint64_t timed_call_n_flush(uint64_t (*p)(uint64_t, uint64_t))
 {
     uint64_t start, end;
-    asm volatile("fence" ::: "memory");
     start = rdtsc();
-    asm volatile("fence" ::: "memory");
     p(0, 0);
-    asm volatile("fence" ::: "memory");
     end = rdtsc();
-    asm volatile("fence" ::: "memory");
     asm volatile("fence.i" ::: "memory");
     asm volatile("fence" ::: "memory");
     return end - start;
