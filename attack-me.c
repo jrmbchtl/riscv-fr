f119258abd7cc2cb08ba17915c83cd68f858ead9
Jorim <s8jmbech@stud.uni-saarland.de>
2022-04-30 12:33:25 +0200

    minor fix

--- attack-me.c
+++ attack-me.c
@@ -1,11 +1,3 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
-#include "attack-me.h"
-
 uint64_t multiply(uint64_t x, uint64_t y)
 {
     return x * y;
