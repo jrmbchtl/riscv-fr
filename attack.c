7bb523d8207b0b648f8495bb6e8cd8b9fcfa8126
Jorim <s8jmbech@stud.uni-saarland.de>
2022-05-02 09:54:38 +0200

    debug

--- attack.c
+++ attack.c
@@ -44,6 +44,26 @@ uint64_t median(uint64_t* list, uint64_t size) {
     return list[size/2];
 }
 
+uint64_t get_over(uint64_t* list, uint64_t size, uint64_t threshold) {
+    uint64_t over = 0;
+    for (uint64_t i = 0; i < size; i++) {
+        if (list[i] > threshold) {
+            over++;
+        }
+    }
+    return over;
+}
+
+uint64_t get_under(uint64_t* list, uint64_t size, uint64_t threshold) {
+    uint64_t under = 0;
+    for (uint64_t i = 0; i < size; i++) {
+        if (list[i] < threshold) {
+            under++;
+        }
+    }
+    return under;
+}
+
 uint64_t get_threshold(uint64_t (*p)(uint64_t, uint64_t)) {
     uint64_t cached_timings[1024] = {0};
     uint64_t uncached_timings[1024] = {0};
@@ -61,6 +81,8 @@ uint64_t get_threshold(uint64_t (*p)(uint64_t, uint64_t)) {
     printf("Cached median: %lu\n", cached_median);
     printf("Uncached median: %lu\n", uncached_median);
     printf("Threshold: %lu\n", threshold);
+    printf("Cached over: %lu\n", get_over(cached_timings, 1024, threshold));
+    printf("Uncached under: %lu\n", get_under(uncached_timings, 1024, threshold));
 
     return threshold;
 }
