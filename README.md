43025bbdbdf812ba8e849b69c19b051f874f0c36
Jorim <s8jmbech@stud.uni-saarland.de>
2022-05-03 10:37:15 +0200

    eviction expreriments

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ This is an example implementation for Flush and Reload on RISC-V, not meant for 
 
 Facts:
  - The 'rdcycle' instruction has a resolution of 1 cycle, different to newer AMD processors (which have a resolution of 36? cycles).
+ - fence.i has issues when used cross thread, gives sub 50% detectioin rate of 1 event on 100 million events.
+ - `getconf PAGESIZE` shows that the page size is 4096 bytes, as also commonly used on other systems
---
+++ evict.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+
+static inline uint64_t rdtsc() {
+  uint64_t val;
+  asm volatile ("rdcycle %0\n":"=r" (val) ::);
+  return val;
+}
+
+static inline maccess(void *p) {
+    uint64_t val;
+    asm volatile("ld %0, %1\n" :"=r" (val) : "m"(p):);
+}
+
+static inline uint64_t timed_load(void *p){
+    uint64_t start, end;
+    start = rdtsc();
+    maccess(p);
+    end = rdtsc();
+    return end-start;
+}
+
+int compare_uint64_t (const void * a, const void * b) 
+{
+   return ( *(int*)a - *(int*)b );
+}
+
+uint64_t median(uint64_t* list, uint64_t size)
+{
+    uint64_t* sorted = malloc(size * sizeof(uint64_t));
+    memcpy(sorted, list, size * sizeof(uint64_t));
+    qsort(sorted, size, sizeof(uint64_t), compare_uint64_t);
+    uint64_t median = sorted[size / 2];
+    free(sorted);
+    return median;
+}
+
+void dummy() {
+    return;
+}
+
+int main() {
+    void* eviction_set[1024] = {0};
+
+    for (int i = 0; i < 1024; i++) {
+        eviction_set[i] = dummy + i * 0x1000;
+    }
+
+    uint64_t cached_timings[1024] = {0};
+
+    maccess(dummy);
+    for (int i = 0; i < 1024; i++) {
+        cached_timings[i] = timed_load(dummy);
+    }
+    uint64_t cached_timing = median(cached_timings, 1024);
+    printf("Cached timing: %lu\n", cached_timing);
+
+}
