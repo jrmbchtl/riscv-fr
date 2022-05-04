1bc41da516b0d826c3523dc56097bb62f1960cba
Jorim <s8jmbech@stud.uni-saarland.de>
2022-05-04 11:21:49 +0200

    debug

--- evict.c
+++ evict.c
@@ -80,6 +80,7 @@ uint64_t test_eviction_set(struct Set eviction_set) {
         for (uint64_t i = 0; i < eviction_set.size; i++) {
             uint64_t time = timed_load(eviction_set.list[i]);
             if (time < THRESHOLD) {
+                printf("index %lu failed with time %lu\n", i, time);
                 return 0;
             }
         }
