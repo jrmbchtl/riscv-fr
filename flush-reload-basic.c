d7ceb4edf6226107d4ed59986ca2735b826567da
Jorim <s8jmbech@stud.uni-saarland.de>
2022-05-11 10:43:11 +0200

    debug

--- flush-reload-basic.c
+++ flush-reload-basic.c
@@ -182,8 +182,9 @@ int main()
 
         while(done == 0)
         {
-            sample_t sq_timing = timed_call_1(square);
+            
             sample_t mul_timing = timed_call_2(multiply);
+            sample_t sq_timing = timed_call_1(square);
             flush();
             
             if (mul_timing.duration < threshold_2)
