29795f8baf8afe6e57be5f3f55b9adf5ad4fc4db
Jorim <s8jmbech@stud.uni-saarland.de>
2022-05-27 13:19:09 +0200

    added timer test

---
+++ timer-test.c
@@ -0,0 +1,37 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define RUNS 1000
+
+int main()
+{
+    // open rdcycle.csv
+    FILE *fp = fopen("rdcycle.csv", "w");
+    if (fp == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+    for (int i = 0; i < RUNS; i++) {
+        uint64_t start, end;
+        asm volatile("rdcycle %0\n" : "=r"(start)::);
+        asm volatile("rdcycle %0\n" : "=r"(end)::);
+        fprintf(fp, "%lu\n", end - start);
+    }
+    fclose(fp);
+
+    // open rdtime.csv
+    fp = fopen("rdtime.csv", "w");
+    if (fp == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+    for (int i = 0; i < RUNS; i++) {
+        uint64_t start, end;
+        asm volatile("rdtime %0\n" : "=r"(start)::);
+        asm volatile("rdtime %0\n" : "=r"(end)::);
+        fprintf(fp, "%lu\n", end - start);
+    }
+
+    return 0;
+}
