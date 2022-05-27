47265dc2b3a971538d4cbc818926fa705accaa51
Jorim <s8jmbech@stud.uni-saarland.de>
2022-05-27 14:54:54 +0200

    asm fix

--- modify-mxstatus.c
+++ modify-mxstatus.c
@@ -4,10 +4,10 @@
 int main() {
     // get current value of mxstatus register with inline asm
     uint64_t mxstatus;
-    asm volatile("mrs %0, mxstatus" : "=r"(mxstatus));
+    asm volatile("ld %0, mxstatus" : "=r"(mxstatus));
     mxstatus |= 0x1 << 22; // set bit 22 to 1
     // write new value to mxstatus register with inline asm
-    asm volatile("msr mxstatus, %0" :: "r"(mxstatus));
+    asm volatile("sd mxstatus, %0" :: "r"(mxstatus));
 
 
     return 0;
