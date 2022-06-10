010436b15633774fd8260bcd298fa1972179b68a
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-10 11:09:38 +0200

    cleanup

--- read_mcycle/read_mcycle_module.c
+++ read_mcycle/read_mcycle_module.c
@@ -10,12 +10,12 @@ MODULE_DESCRIPTION(“A simple example Linux module.”);
 MODULE_VERSION(“0.01”);
 
 static int __init read_mcycle_module_init(void) {
-    printk(KERN_INFO “Hello, World!\n”);
+    printk(KERN_INFO "Hello, World!\n");
     return 0;
 }
 
 static void __exit read_mcycle_module_exit(void) {
-    printk(KERN_INFO “Goodbye, World!\n”);
+    printk(KERN_INFO "Goodbye, World!\n");
 }
 
 module_init(read_mcycle_module_init);
