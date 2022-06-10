6d6bd0494e33e8916752b52dd5ae20cfd7be9970
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-10 14:00:32 +0200

    finalizing stuff

--- analyze_sq_mul.py
+++ analyze_sq_mul.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+# read data from square.csv and multiply.csv
 with open('square.csv', 'r') as f:
     sq = f.readlines()
 
@@ -13,6 +14,7 @@ with open('multiply.csv', 'r') as f:
 sq_data = []
 mul_data = []
 
+# remove all empty lines and convert to int
 for i in range(len(sq)):
     try:
         sq_data.append(int(sq[i].strip()))
@@ -24,6 +26,7 @@ for i in range(len(mul)):
     except:
         pass
 
+# draw all data into a histogram
 mul_data = pd.DataFrame(mul_data, columns=['multiply'])
 ax = mul_data.plot(kind='hist', bins=100, title='multiply')
 sq_data = pd.DataFrame(sq_data, columns=['square'])
--- flush-reload-basic.c
+++ flush-reload-basic.c
@@ -5,7 +5,6 @@
 #include <string.h>
 #include <unistd.h>
 
-
 #define SAMPLE_SIZE     10000
 #define RUNS            16000
 
@@ -14,17 +13,7 @@ typedef struct {
     uint64_t duration;
 } sample_t;
 
-/*
- Other processors may implement the fence.i instruction differently and flush the
- cache. Our processor does not do this. This PoC can still be used to test for the
- specific fence implementation, if it works the implementation is flush based.
-
- For deatails see:
-    - Flush instruction in RISC-V Instruction set manual
-    - https://elixir.bootlin.com/linux/v4.15.5/source/arch/riscv/include/asm/cacheflush.h
-*/
-
-// ---------------------------------------------------------------------------
+// funtcion equivalent to rdtsc on x86, but implemented on RISC-V
 static inline uint64_t rdtsc()
 {
     uint64_t val;
@@ -32,32 +21,14 @@ static inline uint64_t rdtsc()
     return val;
 }
 
-// ---------------------------------------------------------------------------
-// On a simple risc-v implementation this might flush the cache
-// On our processor this is not sufficient -> eviction sets are necessary
+// function to flush the I-cache
 static inline void flush()
 {
     asm volatile("fence.i" ::: "memory");
     asm volatile("fence" ::: "memory");
 }
 
-// ---------------------------------------------------------------------------
-static inline void maccess(void *p)
-{
-    uint64_t val;
-    asm volatile("ld %0, %1\n" : "=r"(val) : "m"(p) :);
-}
-
-// ---------------------------------------------------------------------------
-static inline sample_t timed_load(void *p)
-{
-    uint64_t start, end;
-    start = rdtsc();
-    maccess(p);
-    end = rdtsc();
-    return (sample_t) {start, end - start};
-}
-
+// measure the time it takes to execute function p(0) and return start and duration
 static inline sample_t timed_call_1(uint64_t (*p)(uint64_t))
 {
     uint64_t start, end;
@@ -67,6 +38,7 @@ static inline sample_t timed_call_1(uint64_t (*p)(uint64_t))
     return (sample_t) {start, end - start};
 }
 
+// measure the time it takes to execute function p(0,0) and return start and duration
 static inline sample_t timed_call_2(uint64_t (*p)(uint64_t, uint64_t))
 {
     uint64_t start, end;
@@ -76,36 +48,19 @@ static inline sample_t timed_call_2(uint64_t (*p)(uint64_t, uint64_t))
     return (sample_t) {start, end - start};
 }
 
-static inline sample_t timed_call_n_flush_1(uint64_t (*p)(uint64_t))
-{
-    uint64_t start, end;
-    start = rdtsc();
-    p(0);
-    end = rdtsc();
-    flush();
-    return (sample_t) {start, end - start};
-}
-
-static inline sample_t timed_call_n_flush_2(uint64_t (*p)(uint64_t, uint64_t))
-{
-    uint64_t start, end;
-    start = rdtsc();
-    p(0, 0);
-    end = rdtsc();
-    flush();
-    return (sample_t) {start, end - start};
-}
-
+// simple function multiplying two numbers
 uint64_t multiply(uint64_t x, uint64_t y)
 {
     return x * y;
 }
 
+// simple function squaring a number
 uint64_t square(uint64_t x)
 {
     return x * x;
 }
 
+// victim function that calls square and multiply 10 times with usleeps inbetween
 void* calculate(void* d)
 {
     size_t* done = (size_t*)d;
@@ -120,11 +75,14 @@ void* calculate(void* d)
     *done = 1;
 }
 
+// compare function for qsort
 int compare_uint64_t (const void * a, const void * b) 
 {
    return ( *(int*)a - *(int*)b );
 }
 
+// returns the median of a list given a list and its length
+// works by sorting the list and returning the middle element
 uint64_t median(uint64_t* list, uint64_t size)
 {
     uint64_t* sorted = malloc(size * sizeof(uint64_t));
@@ -137,90 +95,94 @@ uint64_t median(uint64_t* list, uint64_t size)
 
 int main()
 {
-    // No pthreads on user level riscv so we do a simple poc
-    uint64_t timing = 0;
+    // cached timing arrays for square and multiply
     uint64_t chached_timings_1[SAMPLE_SIZE] = {0};
     uint64_t chached_timings_2[SAMPLE_SIZE] = {0};
+    // uncached timing arrays for square and multiply
     uint64_t unchached_timings_1[SAMPLE_SIZE] = {0};
     uint64_t unchached_timings_2[SAMPLE_SIZE] = {0};
+    // thresholds for square and multiply
     uint64_t threshold_1 = 0;
     uint64_t threshold_2 = 0;
-    pthread_t spam;
+    // victim thread
+    pthread_t calculate_thread;
 
-    // get threshold for cached and uncached multiply access
+    // get threshold for cached and uncached square access
     square(0);
     for (size_t i=0; i<SAMPLE_SIZE; i++) {
         chached_timings_1[i] = timed_call_1(square).duration;
     }
     for (size_t i=0; i<SAMPLE_SIZE; i++) {
-        unchached_timings_1[i] = timed_call_n_flush_1(square).duration;
+        flush();
+        unchached_timings_1[i] = timed_call_1(square).duration;
     }
-    printf("cached median 1 = %lu\n", median(chached_timings_1, SAMPLE_SIZE));
-    printf("uncached median 1 = %lu\n", median(unchached_timings_1, SAMPLE_SIZE));
-    threshold_1 = (median(chached_timings_1, SAMPLE_SIZE) + median(unchached_timings_1, SAMPLE_SIZE))/2;
+    uint64_t cached_median_1 = median(chached_timings_1, SAMPLE_SIZE);
+    uint64_t uncached_median_1 = median(unchached_timings_1, SAMPLE_SIZE);
+    threshold_1 = (uncached_median_1 + cached_median_1)/2;
     printf("threshold 1: %lu\n", threshold_1);
 
+    // get threshold for cached and uncached multiply access
     multiply(0, 0);
-    for (int i = 0; i < 10000; i++)
+    for (int i = 0; i < SAMPLE_SIZE; i++)
     {
         chached_timings_2[i] = timed_call_2(multiply).duration;
     }
-    for (int i = 0; i < 10000; i++)
+    for (int i = 0; i < SAMPLE_SIZE; i++)
     {
         flush();
         unchached_timings_2[i] = timed_call_2(multiply).duration;
     }
-    printf("cached median 2 = %lu\n", median(chached_timings_2, 10000));
-    printf("uncached median 2 = %lu\n", median(unchached_timings_2, 10000));
-    threshold_2 = (median(chached_timings_2, 10000) + median(unchached_timings_2, 10000))/2;
+    uint64_t cached_median_2 = median(chached_timings_2, SAMPLE_SIZE);
+    uint64_t uncached_median_2 = median(unchached_timings_2, SAMPLE_SIZE);
+    threshold_2 = (uncached_median_2 + cached_median_2)/2;
     printf("threshold 2: %lu\n", threshold_2);
 
+    // run victim thread RUNS times and observe the accesses to square
+    // results are written to square.csv
     printf("Observing square...\n");
     FILE* sq = fopen("square.csv", "w");
     for(size_t i=0; i<RUNS; i++) {
         size_t done = 0;
-        pthread_create(&spam, NULL, calculate, &done);
+        pthread_create(&calculate_thread, NULL, calculate, &done);
         uint64_t start = rdtsc();
         flush();
 
         while(done == 0)
         {   
-
             sample_t sq_timing = timed_call_1(square);
+            // flush after call to reduce chance of access between measurement and flush
             flush();
-            
             if (sq_timing.duration < threshold_1)
             {
                 fprintf(sq, "%lu\n", sq_timing.start - start);
             }
         }
-        pthread_join(spam, NULL);
+        pthread_join(calculate_thread, NULL);
     }
     fclose(sq);
-
     printf("Done observing square\n");
 
+    // run victim thread RUNS times and observe the accesses to multiply
+    // results are written to multiply.csv
     printf("Observing multiply...\n");
-
     FILE* mul = fopen("multiply.csv", "w");
     for(size_t i=0; i<RUNS; i++) {
         size_t done = 0;
-        pthread_create(&spam, NULL, calculate, &done);
+        pthread_create(&calculate_thread, NULL, calculate, &done);
         uint64_t start = rdtsc();
         flush();
 
         while(done == 0)
         {   
-
             sample_t mul_timing = timed_call_2(multiply);
+            // flush after call to reduce chance of access between measurement and flush
             flush();
-
             if (mul_timing.duration < threshold_2)
             {
                 fprintf(mul, "%lu\n", mul_timing.start - start);
             }
         }
-        pthread_join(spam, NULL);
+        pthread_join(calculate_thread, NULL);
     }
     fclose(mul);
     printf("Done observing multiply\n");
