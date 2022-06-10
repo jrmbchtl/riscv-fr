609a505cb5e4f504928bd5a20f119cda1c673fc8
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-10 09:27:00 +0200

    added reader

--- modify-mxstatus.c
+++ modify-mxstatus.c
@@ -2,13 +2,9 @@
 #include <stdint.h>
 
 int main() {
-    // get current value of mxstatus register with inline asm
-    uint64_t mxstatus;
-    asm volatile("ld %0, mxstatus" : "=r"(mxstatus));
-    mxstatus |= 0x1 << 22; // set bit 22 to 1
-    // write new value to mxstatus register with inline asm with sd
-    asm volatile("sd mxstatus,%0" : "=r"(mxstatus));
-
+    register int i asm("mxstatus");
+    // print i has hex
+    printf("%x\n", i);
 
     return 0;
 }
