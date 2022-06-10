d2b5e88a02fcb568676303c8479c7600d4f8f6c8
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-10 09:48:35 +0200

    debug

--- modify-mxstatus.c
+++ modify-mxstatus.c
@@ -4,7 +4,7 @@
 int main() {
     uint64_t fallback = 0;
     uint64_t val;
-    asm volatile("csrrs %0, cycle[h], %1": "=r"(val), "=r"(fallback)::);
+    asm volatile("csrrs %0, cycle, %1": "=r"(val), "=r"(fallback)::);
     printf("%llu\n", val);
 
     // register int i asm("a2");
