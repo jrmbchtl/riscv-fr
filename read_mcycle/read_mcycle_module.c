f8e2bd17a88208e0cf5f41c08e34972f35d7d46e
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-10 10:05:35 +0200

    renaming

--- read_mcycle/read_mcycle_module.c
+++ read_mcycle/read_mcycle_module.c
@@ -2,20 +2,21 @@
 
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/kernel.h>MODULE_LICENSE(“GPL”);
+#include <linux/kernel.h>
 
+MODULE_LICENSE(“GPL”);
 MODULE_AUTHOR(“Jorim Bechtle”);
 MODULE_DESCRIPTION(“A simple example Linux module.”);
 MODULE_VERSION(“0.01”);
 
-static int __init lkm_example_init(void) {
+static int __init read_mcycle_module_init(void) {
     printk(KERN_INFO “Hello, World!\n”);
     return 0;
 }
 
-static void __exit lkm_example_exit(void) {
+static void __exit read_mcycle_module_exit(void) {
     printk(KERN_INFO “Goodbye, World!\n”);
 }
 
-module_init(lkm_example_init);
-module_exit(lkm_example_exit);
+module_init(read_mcycle_module_init);
+module_exit(read_mcycle_module_exit);
