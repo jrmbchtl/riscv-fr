ddcaace49545054f1004d32d10836ffef4b5bce7
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-13 15:45:35 +0200

    added flush

--- read_mcycle/read_mcycle_module.c
+++ read_mcycle/read_mcycle_module.c
@@ -12,7 +12,9 @@ MODULE_VERSION("0.01");
 static int __init read_mcycle_module_init(void) {
     uint64_t val = 0;
     asm volatile("csrrs %0, 0x5C0, x0": "=r"(val)::);
+    asm volatile (".byte 0x0b, 0x00, 0x20, 0x00\n":::);
     printk(KERN_INFO "0x%lx\n", val);
+
     return 0;
 }
 
