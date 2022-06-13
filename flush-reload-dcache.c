8f1decb8620cc37bfc52ffbdd54fb5b2fff10baf
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-13 15:03:08 +0200

    added dcache poc

---
+++ flush-reload-dcache.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define SIZE     4096
+
+unsigned long lookuptable[SIZE] = {0};
+
+// funtcion equivalent to rdtsc on x86, but implemented on RISC-V
+static inline uint64_t rdtsc()
+{
+    uint64_t val;
+    asm volatile("rdcycle %0\n" : "=r"(val)::);
+    return val;
+}
+
+static inline void maccess(void *p){
+    uint64_t val;
+    asm volatile("ld %0, %1\n" :"=r" (val) : "m"(p):);
+}
+
+static inline uint64_t timed_load(void *p){
+    uint64_t start, end;
+    start = rdtsc();
+    maccess(p);
+    end = rdtsc();
+    return end-start;
+} 
+
+int main() {
+    uint64_t timings[SIZE] = {0};
+    for (int i = 0; i < SIZE; i++) {
+        timings[i] = timed_load(&lookuptable[i]);
+    }
+    return 1;
+}
