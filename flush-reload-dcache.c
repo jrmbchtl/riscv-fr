682da738d015ea67747e5b62a9691a76e1066e47
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-14 09:24:23 +0200

    added fence

--- flush-reload-dcache.c
+++ flush-reload-dcache.c
@@ -18,6 +18,7 @@ static inline uint64_t rdtsc()
 
 static inline void flush(void *p) {
     asm volatile("ld %0, %1\n" : "=r"(p): "m"(p): "a5");
+    asm volatile("fence" ::: "memory");
     asm volatile (".byte 0x0b, 0x78, 0x70, 0x03\n":::);
     printf("%p\n", p);
 }
