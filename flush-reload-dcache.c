7a1ea2fa87842343fbbdc2a545bf8fa137ef0ba0
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-14 12:02:10 +0200

    debug

--- flush-reload-dcache.c
+++ flush-reload-dcache.c
@@ -67,43 +67,15 @@ int main() {
     void* addresses[SIZE] = {0};
 
     for (int i = 0; i < SIZE; i++) {
-        addresses[i] = &p[i];
-    }
-
-    for (int i = 0; i < SIZE; i++) {
         flush(addresses[i]);
-    }
-
-    for (int i = 0; i < SIZE; i++) {
-        timings[i] = timed_load(addresses[i]);
-    }
-    // open cache_hits.csv
-    FILE *fp = fopen("cache_hits.csv", "w");
-    for (int i = 0; i < SIZE; i++) {
-        fprintf(fp, "%lu\n", timings[i]);
-    }
-    fclose(fp);
-    // now with flushing
-    printf("Now with flushing\n");
-    for (int i = 0; i < SIZE; i++) {
-        flush(addresses[i]);
-        // flush_all(addresses, SIZE);
         timings[i] = timed_load(addresses[i]);
     }
     // open cache_misses.csv
-    fp = fopen("cache_misses.csv", "w");
     for (int i = 0; i < SIZE; i++) {
-        fprintf(fp, "%lu\n", timings[i]);
         if (timings[i] > 30) {
             printf("%d: %lu: %p\n", i, timings[i], addresses[i]);
         }
     }
-    fclose(fp);
-    free(p);
-
-    // calibrate_offset();
-    // printf("Done\n");
-    // flush(1);
-    
-    return 1;
+    free(p);    
+    return 0;
 }
