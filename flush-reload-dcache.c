80c10812776a208677966c49f9d58fff0d1773aa
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-14 11:09:45 +0200

    debug

--- flush-reload-dcache.c
+++ flush-reload-dcache.c
@@ -19,7 +19,7 @@ static inline uint64_t rdtsc()
 static inline void flush(void *p) {
     uint64_t val;
     // load value of p into register a5
-    asm volatile("ld a5, %0\n;word 0x0277800b\n" :: "m"(p):);
+    asm volatile("ld a5, %0\n;.word 0x0277800b\n" :: "m"(p):);
     // dcache.civa with a5 as input
     // asm volatile (".word 0x0277800b\n":::);
 }
