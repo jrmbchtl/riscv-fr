f7756da948e84156a6e4f1afc45e258cd998320d
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-14 13:55:33 +0200

    debug

--- flush-reload-dcache.c
+++ flush-reload-dcache.c
@@ -6,7 +6,7 @@
 
 #define SIZE     16384
 #define CALIBRATE  16384
-unsigned char lookuptable[SIZE] = {0};
+char __attribute__((aligned(4096))) data[4096 * 4];
 
 // funtcion equivalent to rdtsc on x86, but implemented on RISC-V
 static inline uint64_t rdtsc()
@@ -19,8 +19,8 @@ static inline uint64_t rdtsc()
 static inline void flush(void *p) {
     uint64_t val;
     // load p into a5 and flush the dcache line with this address
-    asm volatile("ld a5, %0\n;.word 0x0277800b\n" :: "m"(p):);
-    // asm volatile("mv a5, %0; .word 0x0277800b\n" : : "r"(p) :"a5","memory");
+    // asm volatile("ld a5, %0\n;.word 0x0277800b\n" :: "m"(p):);
+    asm volatile("mv a5, %0; .word 0x0277800b\n" : : "r"(p) :"a5","memory");
 }
 
 static inline void flush_all(void** list, size_t size) {
@@ -48,14 +48,14 @@ uint64_t calibrate_offset()
 
     for (int i = 0; i < CALIBRATE; i++)
     {
-        flush(&lookuptable[i]);
-        timings[i] = timed_load(&lookuptable[i]);
+        flush(&data[i]);
+        timings[i] = timed_load(&data[i]);
     }
 
     for (int i = 0; i < CALIBRATE; i++)
     {
         if (timings[i] > 100) {
-            printf("%d: %lu: %p\n", i, timings[i], &lookuptable[i]);
+            printf("%d: %lu: %p\n", i, timings[i], &data[i]);
             
         }
     }
@@ -65,8 +65,10 @@ int main() {
     uint64_t timings[SIZE] = {0};
     void* addresses[SIZE] = {0};
 
+    memset(data, 0, 4096 * 4);
+
     for (int i = 0; i < SIZE; i++) {
-        addresses[i] = &lookuptable[i];
+        addresses[i] = &data[i];
     }
 
     int initial = 0;
@@ -80,8 +82,8 @@ int main() {
     printf("%p\n", addresses[initial]);
 
     for (int i = 0; i < SIZE; i++) {
-        flush(&lookuptable[i]);
-        timings[i] = timed_load(&lookuptable[i]);
+        flush(&data[i]);
+        timings[i] = timed_load(&data[i]);
     }
     for (int i = 0; i < SIZE; i++) {
         if (timings[i] > 30) {
