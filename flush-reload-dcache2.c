80e7f90ff4f85a6651743f3ce02634264647e14b
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-15 09:51:55 +0200

    debug

--- flush-reload-dcache2.c
+++ flush-reload-dcache2.c
@@ -71,7 +71,7 @@ int main() {
     for (int i = 0; i < SIZE; i++)
     {
         if (timings[i] > 30) {
-            printf("%d, %lu, %p\n", i, timings[i], addresses[i]);
+            // printf("%d, %lu, %p\n", i, timings[i], addresses[i]);
         }
     }
 
