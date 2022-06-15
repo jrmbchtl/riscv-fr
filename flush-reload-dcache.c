bea4a77216fe838a5f6ccd83d89db7159f1b41ec
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-15 14:42:53 +0200

    debug

--- flush-reload-dcache.c
+++ flush-reload-dcache.c
@@ -62,7 +62,7 @@ void* calculate(void* d)
 int main()
 {
     void *address = &data[0];
-    uint64_t timings[17] = {0};
+    uint64_t timings[16] = {0};
     char tmp = data[0];
     uint64_t timing = timed_load(address);
     printf("This should be low: %lu\n", timing);
