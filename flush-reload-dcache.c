8cd665f09ade2d0c98aa07497d3d1f0b6a1d59b2
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-15 11:32:23 +0200

    debug

--- flush-reload-dcache.c
+++ flush-reload-dcache.c
@@ -88,17 +88,17 @@ int main() {
     }
 
     // open log.csv
-    FILE* fp = fopen("log.csv", "w");
+    
     for (int i = 0; i < SIZE; i++) {
         if (((uint64_t) addresses[access_pattern[i]]) % OFFSET != 0) {
             continue;
         }
-        fprintf(fp, "%d\n", i);
         flush(addresses[access_pattern[i]]);
         timings[access_pattern[i]] = timed_load(addresses[access_pattern[i]]);
     }
-    fclose(fp);
 
+
+    
     for (int i = 0; i < SIZE; i++)
     {
         if (timings[i] > 20) {
