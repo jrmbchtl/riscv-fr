ccc6dbe60dda721a6ad4096dae08191aa73df923
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-15 10:40:25 +0200

    debug

--- flush-reload-dcache.c
+++ flush-reload-dcache.c
@@ -5,8 +5,11 @@
 #include <unistd.h>
 
 #define SIZE     16384
-#define MAX_SIZE 20
+#define OFFSET   16
 char __attribute__((aligned(4096))) data[4096 * 4];
+void* max_addr = &data[SIZE-1];
+
+uint64_t access_pattern[SIZE] = {0};
 
 // funtcion equivalent to rdtsc on x86, but implemented on RISC-V
 static inline uint64_t rdtsc()
@@ -18,28 +21,19 @@ static inline uint64_t rdtsc()
 
 static inline void flush(void *p) {
     uint64_t val;
+    void* p_new;
+    if (OFFSET - ((uint64_t)p % OFFSET) == OFFSET) {
+        p_new = p;
+    } else {
+        p_new = p - ((uint64_t)p % OFFSET) + OFFSET;
+    }
+    if(p_new > max_addr) {
+        p_new = p;
+    }
+
     // load p into a5 and flush the dcache line with this address
     // asm volatile("ld a5, %0\n;.word 0x0277800b\n" :: "m"(p):);
-    asm volatile("mv a5, %0; .word 0x0277800b\n" : : "r"(p) :"a5","memory");
-}
-
-static inline void flush_offset(void *p, uint64_t offset) {
-    uint64_t val;
-    uint64_t p1 = (uint64_t)p % 0x800;
-    uint64_t p2 = (uint64_t)p - p1 + offset;
-    if(p2 > (uint64_t)p) {
-        p2 -= 0x800;
-    }
-    // printf("p: %lx, p2: %lx\n", p, p2);
-    // load p into a5 and flush the dcache line with this address
-    // asm volatile("ld a5, %0\n;.word 0x0277800b\n" :: "m"(p):);
-    asm volatile("mv a5, %0; .word 0x0277800b\n" : : "r"(p) :"a5","memory");
-}
-
-static inline void flush_all(void** list, size_t size) {
-    for (int i = 0; i < size; i++) {
-        flush(list[i]);
-    }
+    asm volatile("mv a5, %0; .word 0x0277800b\n" : : "r"(p_new) :"a5","memory");
 }
 
 static inline void maccess(void *p) {
@@ -53,58 +47,27 @@ static inline uint64_t timed_load(void *p){
     maccess(p);
     end = rdtsc();
     return end-start;
-} 
-
-uint64_t vote(uint64_t* list, size_t size) {
-    int numbers[size];
-    uint64_t options[size];
-    uint64_t list_tmp[size];
-    int option_count = 0;
-
-    for (int i = 0; i < size; i++) {
-        list_tmp[i] = list[i] % 2048;
-    }
-
-    for (int i = 0; i < size; i++) {
-        int found = 0;
-        for (int j = 0; j < option_count; j++) {
-            if (options[j] == list_tmp[i]) {
-                numbers[j]++;
-                found = 1;
-                break;
-            }
-        }
-        if (found) {
-            continue;
-        }
-        options[option_count] = list_tmp[i];
-        numbers[option_count] = 1;
-        option_count++;
-    }
-
-    int max = 0;
-    int max_index = 0;
-    for (int i = 0; i < option_count; i++) {
-        if (numbers[i] > max) {
-            max = numbers[i];
-            max_index = i;
-        }
-    }
-    return options[max_index];
 }
 
-uint64_t calibrate_offset()
-{
-    
-    // return offset;
+void shuffle_list(uint64_t* list, size_t size) {
+    // randomize the list
+    for (int i = 0; i < size; i++) {
+        int j = rand() % size;
+        uint64_t tmp = list[i];
+        list[i] = list[j];
+        list[j] = tmp;
+    }
 }
 
 int main() {
     uint64_t timings[SIZE] = {0};
     void* addresses[SIZE] = {0};
 
-    uint64_t relevant_addresses[MAX_SIZE] = {0};
-    size_t size = 0;
+    for (uint64_t i = 0; i < SIZE; i++) {
+        access_pattern[i] = i;
+    }
+
+    shuffle_list(access_pattern, SIZE);
 
     memset(data, 0, 4096 * 4);
 
@@ -113,29 +76,8 @@ int main() {
     }
 
     for (int i = 0; i < SIZE; i++) {
-        flush(addresses[i]);
-        timings[i] = timed_load(addresses[i]);
-    }
-
-    for (int i = 0; i < SIZE; i++)
-    {
-        if (timings[i] > 30) {
-            relevant_addresses[size++] = (uint64_t) addresses[i];
-        }
-    }
-
-    for (int i = 0; i < size; i++)
-    {
-        printf("%p\n", relevant_addresses[i]);
-    }
-
-    uint64_t offset = vote(relevant_addresses, size);
-    printf("offset: %lx\n", offset);
-
-    for (int i = 0; i < SIZE; i++)
-    {
-        flush_offset(addresses[i], offset);
-        timings[i] = timed_load(addresses[i]);
+        flush(addresses[access_pattern[i]]);
+        timings[access_pattern[i]] = timed_load(addresses[access_pattern[i]]);
     }
 
     for (int i = 0; i < SIZE; i++)
@@ -143,7 +85,7 @@ int main() {
         if (timings[i] > 30) {
             printf("%d, %lu, %p\n", i, timings[i], addresses[i]);
         }
-    }   
+    }
 
     return 0;
 }
