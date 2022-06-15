d69d77e561e4f15c476f875276622e0f58273d34
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-15 09:23:14 +0200

    debug

--- flush-reload-dcache.c
+++ flush-reload-dcache.c
@@ -138,12 +138,13 @@ int main() {
         timings[i] = timed_load(addresses[i]);
     }
 
-    // open cache_miss.csv
-    FILE* f = fopen("cache_miss.csv", "w");
     for (int i = 0; i < SIZE; i++)
     {
-        fprintf(f, "%d, %lx\n", i, timings[i]);
+        if (timings[i] > 30) {
+            printf("%d, %lu, %p\n", i, timings[i], addresses[i]);
+        }
+    }   
+        }
     }
-
     return 0;
 }
