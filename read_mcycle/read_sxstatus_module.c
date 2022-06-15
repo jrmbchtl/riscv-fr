1baa8eb0e576c00c790379389f52dfc5b59f0771
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-15 08:47:18 +0200

    changed to sxstatus

--- read_mcycle/read_sxstatus_module.c
+++ read_mcycle/read_sxstatus_module.c
@@ -1,26 +1,22 @@
-// 0xB00
-
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Jorim Bechtle");
-MODULE_DESCRIPTION("A simple example Linux module.");
+MODULE_DESCRIPTION("A simple RISC-V module reading sxtatus.");
 MODULE_VERSION("0.01");
 
-static int __init read_mcycle_module_init(void) {
+static int __init read_sxstatus_module_init(void) {
     uint64_t val = 0;
     asm volatile("csrrs %0, 0x5C0, x0": "=r"(val)::);
-    asm volatile (".byte 0x0b, 0x00, 0x20, 0x00\n":::);
     printk(KERN_INFO "0x%lx\n", val);
-
     return 0;
 }
 
-static void __exit read_mcycle_module_exit(void) {
+static void __exit read_sxstatus_module_exit(void) {
     printk(KERN_INFO "Goodbye, World!\n");
 }
 
-module_init(read_mcycle_module_init);
-module_exit(read_mcycle_module_exit);
+module_init(read_sxstatus_module_init);
+module_exit(read_sxstatus_module_exit);
