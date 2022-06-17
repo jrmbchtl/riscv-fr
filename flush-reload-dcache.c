94bd883bd7c7410325e042ba84861cad87025608
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-17 09:37:09 +0200

    debug

--- flush-reload-dcache.c
+++ flush-reload-dcache.c
@@ -60,44 +60,10 @@ int main()
     uint64_t timing_low, timing_high, threshold;
 
     flush(address);
-    flush(address + 64);
-    flush(address + 128);
-    flush(address + 192);
-    flush(address + 256);
-    flush(address + 320);
-    flush(address + 384);
-    flush(address + 448);
-    flush(address + 512);
-    flush(address + 576);
-    flush(address + 640);
-    flush(address + 704);
-    flush(address + 768);
-    flush(address + 832);
-    flush(address + 896);
-    flush(address + 960);
-    flush(address + 1024);
-    flush(address);
     timing_high = timed_load(address);
     printf("%lu\n", timing_high);
     timing_low = timed_load(address);
     printf("%lu\n", timing_low);
-    flush(address);
-    flush(address + 64);
-    flush(address + 128);
-    flush(address + 192);
-    flush(address + 256);
-    flush(address + 320);
-    flush(address + 384);
-    flush(address + 448);
-    flush(address + 512);
-    flush(address + 576);
-    flush(address + 640);
-    flush(address + 704);
-    flush(address + 768);
-    flush(address + 832);
-    flush(address + 896);
-    flush(address + 960);
-    flush(address + 1024);
     flush(address);
     timing_high = timed_load(address);
     printf("%lu\n", timing_high);
@@ -107,18 +73,18 @@ int main()
 
     printf("threshold: %lu\n", threshold);
 
-    size_t done = 0;
-    pthread_t do_stuff;
+    // size_t done = 0;
+    // pthread_t do_stuff;
 
-    pthread_create(&do_stuff, NULL, calculate, &done);
-    while (!done) {
-        flush(address);
-        uint64_t timing = timed_load(address);
-        if (timing > threshold) {
-            printf("%lu: %p\n", timing, address);
-        }
-    }
-    pthread_join(do_stuff, NULL);
+    // pthread_create(&do_stuff, NULL, calculate, &done);
+    // while (!done) {
+    //     flush(address);
+    //     uint64_t timing = timed_load(address);
+    //     if (timing > threshold) {
+    //         printf("%lu: %p\n", timing, address);
+    //     }
+    // }
+    // pthread_join(do_stuff, NULL);
 
     return 0;
 }
