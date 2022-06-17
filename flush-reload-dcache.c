768c36179b2ceae7f3bc11ffdbff7ddd77b7ba2f
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-17 11:57:03 +0200

    debug

--- flush-reload-dcache.c
+++ flush-reload-dcache.c
@@ -71,12 +71,12 @@ uint64_t median(uint64_t* list, uint64_t size)
 void* calculate(void* d)
 {
     size_t* done = (size_t*)d;
-    char tmp;
+    // char tmp;
 
-    for (size_t i=0; i<10; i++) {
-        usleep(1000);
-        tmp = data[0];
-    }
+    // for (size_t i=0; i<10; i++) {
+    //     usleep(1000);
+    //     tmp = data[0];
+    // }
     usleep(1000);
     *done = 1;
 }
