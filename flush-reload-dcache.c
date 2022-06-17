b3a42e576b0b870559eca32d6d8ce7449fc3dd8e
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-17 13:47:07 +0200

    debug

--- flush-reload-dcache.c
+++ flush-reload-dcache.c
@@ -150,8 +150,8 @@ int main()
             if (timing.duration < threshold) {
                 fprintf(data_0, "%lu\n", timing.start - start);
             }
-            // if (k < 1000000000) k+=0;
-            // else done = 1;
+            if (k < 1000000000) k+=0;
+            else done = 1;
         }
         pthread_join(victim, NULL);
     }
