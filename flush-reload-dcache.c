bb6b38f5475898a090c2bd57090f95ae4ad1a75e
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-17 11:11:56 +0200

    debug

--- flush-reload-dcache.c
+++ flush-reload-dcache.c
@@ -93,7 +93,7 @@ int main()
 
         // should be a cache miss since everything was flushed
         timing_high = timed_load(addresses[i]);
-        printf("This should be a cache miss @ %d: %lu\n", i, timing_high);
+        // printf("This should be a cache miss @ %d: %lu\n", i, timing_high);
         assert(timing_high > timing_low);
         assert(timing_high > 100);
     }
