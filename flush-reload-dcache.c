005d82200ece315545d95aa4ff3ebc54c9d25884
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-17 15:00:44 +0200

    debug

--- flush-reload-dcache.c
+++ flush-reload-dcache.c
@@ -7,7 +7,7 @@
 #include <unistd.h>
 
 #define SIZE        16384
-#define PRIME_RUNS  10000
+#define PRIME_RUNS  5
 // make data span over exactly 4 pages (a 4K))
 char __attribute__((aligned(4096))) data[4096 * 4];
 
