02022d7ffdbe8b31dffcd6b4b8bb539c3cf278f7
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-21 11:40:26 +0200

    debug

--- evict-reload-basic.c
+++ evict-reload-basic.c
@@ -127,7 +127,7 @@ int main() {
         }
         // printf("new len: %lu\n", len);
         // printf("new index: %lu\n", index);
-        printf("test1: %d\n", eviction_test(addresses_evict, len, target));
+        // printf("test1: %d\n", eviction_test(addresses_evict, len, target));
         // printf("test2: %d\n", eviction_test(addresses_evict, len, target));
         assert(eviction_test(addresses_evict, len, target));
     }
