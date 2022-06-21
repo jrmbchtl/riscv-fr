6071473e627d319e65ec0967a1a3b72afbff397a
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-21 09:07:32 +0200

    debug

--- evict-reload-basic.c
+++ evict-reload-basic.c
@@ -7,8 +7,9 @@
 #include <unistd.h>
 
 #define SIZE            16384
+#define THRESHOLD       100
 char __attribute__((aligned(4096))) data[4096 * 4];
-char __attribute__((aligned(4096))) evict[4096 * 64];
+char __attribute__((aligned(4096))) eviction_data[4096 * 64];
 
 typedef struct {
     uint64_t start;
@@ -34,9 +35,42 @@ sample_t timed_load(void* p) {
     return (sample_t) {start, end - start};
 }
 
+void* pop(void** list, size_t len, size_t index) {
+    // pop item at index from list
+    void* item = list[index];
+    for (size_t i = index; i < len - 1; i++) {
+        list[i] = list[i + 1];
+    }
+    list[len - 1] = NULL;
+    return item;
+}
+
+void append(void** list, size_t len, void* item) {
+    // append item to list
+    list[len - 1] = item;
+}
+
+char eviction_test(void** list, size_t len, void* target) {
+    maccess(target);
+    uint64_t cached_timing = timed_load(target).duration;
+    if (cached_timing < THRESHOLD) {
+        return 0;
+    }
+
+    for (int i = 0; i < len; i++) {
+        maccess(list[i]);
+    }
+    uint64_t uncached_timing = timed_load(target).duration;
+    if (uncached_timing < THRESHOLD) {
+        return 0;
+    }
+
+    return 1;
+}
+
 int main() {
     memset(data, 0, sizeof(data));
-    memset(evict, 0, sizeof(evict));
+    memset(eviction_data, 0, sizeof(eviction_data));
     void* addresses_data[SIZE];
     void* addresses_evict[4096];
 
@@ -45,7 +79,7 @@ int main() {
     }
 
     for (int i = 0; i < 4096; i++) {
-        addresses_evict[i] = &evict[i * 64];
+        addresses_evict[i] = &eviction_data[i * 64];
     }
 
     void* target = &data[0];
@@ -58,6 +92,25 @@ int main() {
     uint64_t new_timing = timed_load(target).duration;
     printf("%lu\n", timing);
     printf("%lu\n", new_timing);
+    assert(timing < new_timing);
+    assert(new_timing > 100);
+    assert(timing < 100);
+
+    size_t len = 4096;
+    size_t index = 0;
+    while (index < len) {
+        void* tmp = pop(addresses_evict, len, index);
+        len--;
+        char test = eviction_test(addresses_evict, len, target);
+        if (!test) {
+            append(addresses_evict, len, tmp);
+            len++;
+            index++;
+        }
+        assert(eviction_test(addresses_evict, len, target));
+        printf("new len: %lu\n", len);
+        printf("current index: %lu\n", index);
+    }
 
     return 0;
 }
