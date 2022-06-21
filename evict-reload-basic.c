7126bceb325f7695a5f891b7dbf30e4df2ddaf7f
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-21 14:54:46 +0200

    debug

--- evict-reload-basic.c
+++ evict-reload-basic.c
@@ -48,26 +48,29 @@ int main() {
         addresses_data[i] = &data[i];
     }
 
-    uint64_t target_index = 0;
-    void* target = addresses_data[target_index];
-    // get down to cache line granularity
-    uint64_t tmp = target_index / 64;
-    uint64_t base = target_index % 128;
-    for (int i = 0; i < 4; i ++) {
-        addresses_evict[i] = &eviction_data[(base + i * 128) * 64];
-    }
-    maccess(target);
-    uint64_t cached_timing = timed_load(target).duration;
-    for (int i = 0; i < 4; i++) {
-        maccess(addresses_evict[i]);
-    }
-    uint64_t uncached_timing = timed_load(target).duration;
-    printf("cached timing: %lu\n", cached_timing);
-    printf("uncached timing: %lu\n", uncached_timing);
+    for (int k = 0; k < 128; k++) {
+        uint64_t target_index = 0;
+        void* target = addresses_data[target_index];
+        // get down to cache line granularity
+        uint64_t tmp = target_index / 64;
+        uint64_t base = target_index % 128;
+        for (int i = 0; i < 4; i ++) {
+            addresses_evict[i] = &eviction_data[(base + i * 128) * 64];
+        }
+        maccess(target);
+        uint64_t cached_timing = timed_load(target).duration;
+        for (int i = 0; i < 4; i++) {
+            maccess(addresses_evict[i]);
+        }
+        uint64_t uncached_timing = timed_load(target).duration;
+        printf("cached timing: %lu\n", cached_timing);
+        printf("uncached timing: %lu\n", uncached_timing);
 
-    for (int i = 0; i < 4; i++) {
-        printf("%p\n", addresses_evict[i]);
+        // for (int i = 0; i < 4; i++) {
+        //     printf("%p\n", addresses_evict[i]);
+        // }
     }
+    
 
     return 0;
 }
