8f2f98f199902591a36c8c5e80a3dc871b872a7e
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-21 12:22:17 +0200

    debug

--- evict-reload-basic.c
+++ evict-reload-basic.c
@@ -101,7 +101,7 @@ int main() {
         addresses_evict[i] = &eviction_data[i * 64];
     }
 
-    void* target = &data[128];
+    void* target = &data[196];
     maccess(target);
     uint64_t timing = timed_load(target).duration;
 
