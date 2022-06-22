2840009f5a904ae03fdff70c606df887994215f4
Jorim <s8jmbech@stud.uni-saarland.de>
2022-06-22 09:47:31 +0200

    debug

--- evict-reload-basic.c
+++ evict-reload-basic.c
@@ -55,7 +55,7 @@ int main() {
         uint64_t tmp = target_index / 64;
         uint64_t base = tmp % 128;
         for (int i = 0; i < 4; i ++) {
-            printf("%d\n", (base + i * 128));
+            // printf("%d\n", (base + i * 128));
             addresses_evict[i] = &eviction_data[(base + i * 128) * 64];
         }
         maccess(target);
